137cdb8282e73db1517a31c0dfbb528dbd03aa5d
Moritz Miedler <71124607+MoritzMiedler@users.noreply.github.com>
2021-11-11 20:20:58 +0100

    es funktioniert? i guess? nach 3h oder so? aber nur bei edge!

---
+++ dist/precache-manifest.a5c9596eed76572dd59f950d15b629a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '4ef717e0d53e3a60eff7',
+    url: '/css/app.8caa578c.css',
+  },
+  {
+    revision: '82b9c7a5a3f405032b1db71a25f67021',
+    url: '/img/logo.82b9c7a5.png',
+  },
+  {
+    revision: 'd721f427ea332493189aee128608b376',
+    url: '/index.html',
+  },
+  {
+    revision: '4ef717e0d53e3a60eff7',
+    url: '/js/app.ce8a734a.js',
+  },
+  {
+    revision: 'e4c9092601fc21c62af8',
+    url: '/js/chunk-vendors.a964916c.js',
+  },
+  {
+    revision: '9f18a38bfd9c5c2046c1874b12fe767c',
+    url: '/manifest.json',
+  },
+  {
+    revision: 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    url: '/robots.txt',
+  },
+]);
